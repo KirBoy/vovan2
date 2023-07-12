51b6cf6f5d424390e76b8db14977665da748c137
boychenko <boychenko@tutu.tech>
2023-07-12 11:28:48 +0300

    add spin

---
+++ main.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.wheel__btn');
+const wheel = document.querySelector('.wheel__center');
+const tablet__text1 = document.querySelector('.tablet__text1');
+const tablet__text2 = document.querySelector('.tablet__text2');
+const tablet1__win = document.querySelector('.tablet1__win');
+const tablet2__win = document.querySelector('.tablet2__win');
+
+btn.onclick = () => {
+    if (wheel.classList.contains('spin1')) {
+        wheel.classList.add('spin2');
+        btn.classList.add('wheel__btn--disabled');
+        tablet__text2.classList.add('tablet__text--hidden');
+        setTimeout(() => {
+            btn.classList.remove('wheel__btn--disabled');
+            tablet2__win.classList.add('win--visible')
+        }, 5000)
+        return;
+    }
+    wheel.classList.add('spin1');
+    btn.classList.add('wheel__btn--disabled');
+    tablet__text1.classList.add('tablet__text--hidden');
+    setTimeout(() => {
+        btn.classList.remove('wheel__btn--disabled');
+        tablet1__win.classList.add('win--visible')
+    }, 5000)
+}
